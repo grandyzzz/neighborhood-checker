c4a2ac3b0ec9de88bcca17ab73be6438275c4c4c
grandyzzz <twieank@gmail.com>
2023-08-23 00:29:28 +0200

    feat: polygons editor

--- app/Map.tsx
+++ app/Map.tsx
@@ -1,24 +1,47 @@
 "use client";
-import React from 'react'
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import React, { useState } from "react";
+import {
+  DrawingManager,
+  GoogleMap,
+  Libraries,
+  Polygon,
+  useJsApiLoader,
+} from "@react-google-maps/api";
 
 const containerStyle = {
-  width: '100%',
-  height: '100%'
+  width: "100%",
+  height: "100%",
 };
 
 const center = {
   lat: 45.4640976,
-  lng: 9.1919265
+  lng: 9.1919265,
 };
 
-function Map() {
-  const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || "",
-  })
+const libraries: Libraries = ["drawing"];
 
-  const [map, setMap] = React.useState<google.maps.Map | null>(null)
+interface MapProps {
+  editable: boolean;
+  mode: string;
+}
+
+function Map({ editable, mode }: MapProps) {
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || "",
+    libraries,
+  });
+
+  const [map, setMap] = React.useState<google.maps.Map | null>(null);
+  const [polygons, setPolygons] = useState<google.maps.Polygon[]>([]);
+
+  const onPolygonComplete = (polygon: google.maps.Polygon) => {
+    setPolygons([...polygons, polygon]);
+    console.log('polygon complete');
+    // setEditable(false);
+    console.log(polygon);
+    
+  };
 
   const onLoad = React.useCallback(function callback(map: google.maps.Map) {
     // This is just an example of getting and using the map instance!!! don't just blindly copy!
@@ -29,25 +52,59 @@ function Map() {
 
     transitLayer.setMap(map);
 
-    setMap(map)
-  }, [])
+    setMap(map);
+  }, []);
 
   const onUnmount = React.useCallback(function callback(map: google.maps.Map) {
-    setMap(null)
-  }, [])
+    setMap(null);
+  }, []);
 
   return isLoaded ? (
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={14}
-        onLoad={onLoad}
-        onUnmount={onUnmount}
-      >
-        { /* Child components, such as markers, info windows, etc. */ }
-        <></>
-      </GoogleMap>
-  ) : <></>
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+     options={{
+        mapTypeControl: false,
+        streetViewControl: true,
+        fullscreenControl: false,
+        zoomControl: true,
+        // zoomControlOptions: {
+        //   position: window.google.maps.ControlPosition.RIGHT_CENTER,
+        // },
+     }}
+      zoom={14}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+      onClick={console.log}
+    >
+      <DrawingManager
+        onPolygonComplete={onPolygonComplete}
+        // drawingMode={window.google.maps.drawing.OverlayType.POLYGON}
+        drawingMode={editable ? window.google.maps.drawing.OverlayType.POLYGON : null}
+        options={{
+          drawingControl: true,
+          drawingControlOptions: {
+            position: window.google.maps.ControlPosition.TOP_CENTER,
+            drawingModes: [window.google.maps.drawing.OverlayType.POLYGON],
+          },
+          polygonOptions: {
+            fillColor: mode === 'allowed' ? '#00FF00' : '#FF0000',
+            fillOpacity: 0.5,
+            strokeWeight: 2,
+            // clickable: true,
+            // editable: true,
+            geodesic: true,
+            zIndex: 1,
+          },
+        }}
+      />
+      {polygons.map((polygon, index) => (
+        <Polygon key={index} path={polygon.getPath()} />
+      ))}
+    </GoogleMap>
+  ) : (
+    <></>
+  );
 }
 
-export default React.memo(Map)
+export default React.memo(Map);
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,31 @@
+'use client'
 import { Button } from '@mui/material'
 import Image from 'next/image'
 import Map from './Map'
+import { useState } from 'react'
 
 export default function Home() {
+  const [isEditing, setEditing] = useState(false);
+  const [mode, setMode] = useState('allowed');
+
+  function toggleEditing(selectedMode: string) {
+    return function() {
+      setMode(selectedMode);
+      setEditing(prevEditing => !prevEditing);
+    }
+  }
+
   return (
     <main className="relative h-full w-full">
-      <Map />
+      <div className="absolute w-full top-0 left-0 p-4 z-10">
+        <Button onClick={toggleEditing('allowed')} variant="contained">
+          { isEditing && mode === 'allowed' ? 'Stop Editing' : 'Start Editing Allowed' }
+        </Button>
+        <Button onClick={toggleEditing('denied')} variant="contained">
+          { isEditing && mode === 'denied' ? 'Stop Editing' : 'Start Editing Denied' }
+        </Button>
+      </div>
+      <Map editable={isEditing} mode={mode} />
     </main>
   )
 }
